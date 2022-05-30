31d1e976cb263e591ca1404b2d23b46b26d0f37d
Kim sehyun <dnjfdid14@naver.com>
2022-05-30 22:54:12 +0900

    예제

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # 원소가 모두 3인 (3,4,5)형태의 numpy.array 출력
-# arr = np.full((3, 4, 5), 3)
-# print(arr)
+arr = np.full((3, 4, 5), 3)
+print(arr)
 
 # 정수 -50~50 범위 안의 난수로 이루어진 (4,5)형태의 
 # numpy.array를 출력하고 행을 기준으로 오름차순 정렬과
