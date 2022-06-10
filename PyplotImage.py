30ec12b5c49381995198e5a6ad92c966a2d398a8
Kim sehyun <dnjfdid14@naver.com>
2022-06-10 13:26:49 +0900

    파이썬

---
+++ PyplotImage.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('다운로드.jpeg')
+
+plt.imshow(img)
+plt.show()
--- matplotlib1.py
+++ matplotlib1.py
@@ -10,5 +10,6 @@ plt.plot(x, y1, label="sin")
 plt.plot(x, y2 , linestyle="--", label="cos")
 plt.xlabel("x")
 plt.ylabel("y")
+plt.title('sin & cos') # 제목
 plt.legend()
 plt.show()
